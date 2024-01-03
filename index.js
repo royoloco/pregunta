65b1b1a07093a160cbaea80f6f9bf8a74dda4a94
royoloco <146995855+royoloco@users.noreply.github.com>
2024-01-02 21:38:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Ya estas comprometida')
+    alert('Yo sabia ya')
 });
 
 const noBtn = document.querySelector('#noBtn');
